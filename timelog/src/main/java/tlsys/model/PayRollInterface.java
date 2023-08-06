b155dff634d564a62574784939ae2379aa65ab2e
Santiago-Soto-21 <santisotofajardo@gmail.com>
2023-08-06 19:59:58 -0400

    Created PayRollInterface

---
+++ timelog/src/main/java/tlsys/model/PayRollInterface.java
@@ -0,0 +1,11 @@
+package tlsys.model;
+
+public interface PayRollInterface {
+
+    public PayInfo printPay(PayInfo payInfo);
+
+    public void netFromBrute();
+
+    public void deductionsReport();
+
+}
